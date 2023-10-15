968846ce21c5dbc4c1e98f64d3e68e23c2685b9a
Yoga Bagas <yogabagas69@gmail.com>
2023-10-15 17:26:37 +0700

    UPD: update link image

--- src/App.vue
+++ src/App.vue
@@ -99,14 +99,14 @@
                 class="mx-auto mt-10 grid max-w-lg grid-cols-4 items-center gap-x-8 gap-y-10 sm:max-w-xl sm:grid-cols-6 sm:gap-x-10 lg:mx-0 lg:max-w-none lg:grid-cols-5">
                 <img class="col-span-2 max-h-12 w-full object-contain lg:col-span-1" src="/src/assets/kemenperin.png"
                     alt="Transistor" width="158" height="48">
-                <img class="col-span-2 max-h-12 w-full object-contain lg:col-span-1" src="src/assets/rumahsoal.png"
+                <img class="col-span-2 max-h-12 w-full object-contain lg:col-span-1" src="/src/assets/rumahsoal.png"
                     alt="Reform" width="158" height="48">
-                <img class="col-span-2 max-h-12 w-full object-contain lg:col-span-1" src="src/assets/soalpppk.png"
+                <img class="col-span-2 max-h-12 w-full object-contain lg:col-span-1" src="/src/assets/soalpppk.png"
                     alt="Tuple" width="158" height="48">
                 <img class="col-span-2 max-h-12 w-full object-contain sm:col-start-2 lg:col-span-1"
-                    src="src/assets/kinarya.png" alt="SavvyCal" width="158" height="48">
+                    src="/src/assets/kinarya.png" alt="SavvyCal" width="158" height="48">
                 <img class="col-span-2 col-start-2 max-h-12 w-full object-contain sm:col-start-auto lg:col-span-1"
-                    src="src/assets/castavia.png" alt="Statamic" width="158" height="48">
+                    src="/src/assets/castavia.png" alt="Statamic" width="158" height="48">
             </div>
         </div>
     </div>
